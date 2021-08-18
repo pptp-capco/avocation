4923e49ee52a5c6a2c17ceb6d5d8e814de88c11a
pptp <ash.patil@capco.com>
2021-08-18 11:45:55 +0100

    remove react specific files

--- src/components/ApiError.tsx
+++ src/components/ApiError.tsx
@@ -1,3 +1,5 @@
 export function ApiError() {
-  return <div>{`Sorry, Something went wrong.`}</div>;
+  const isDevEnvironment = process.env.NODE_ENV === 'development';
+  const message = isDevEnvironment ? 'API Error. Either API is down or API returned error' : 'Sorry, something went wrong'
+  return <div>{message}</div>;
 }
