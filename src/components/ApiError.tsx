b80b45d3179d8193e8abc5ab7585d8ffbe8f4347
pptp <ash.patil@capco.com>
2021-08-18 15:38:18 +0100

    create utils

--- src/components/ApiError.tsx
+++ src/components/ApiError.tsx
@@ -1,5 +1,6 @@
-export function ApiError() {
-  const isDevEnvironment = process.env.NODE_ENV === 'development';
-  const message = isDevEnvironment ? 'API Error. Either API is down or API returned error' : 'Sorry, something went wrong'
+import {isDevEnvironment} from "../utils";
+
+export const ApiError = () => {
+  const message = isDevEnvironment() ? 'API Error. Either API is down or API returned error' : 'Sorry, something went wrong'
   return <div>{message}</div>;
-}
+};
