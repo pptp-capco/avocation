f7ef75d093cc5889af6a667af8525385eaae01cb
pptp <ash.patil@capco.com>
2021-09-10 11:26:18 +0100

    add styles

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,31 +1,56 @@
 import React, {useRef, useState} from 'react';
 import {
-    Button,
+    Button, createStyles, makeStyles, TextField, Theme,
 } from "@material-ui/core";
 import {Enquiries} from "../Enquiries";
 import './search.css'
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            '& > *': {
+                margin: theme.spacing(1),
+                width: '25ch',
+            },
+        },
+    }),
+);
+
 export function Search() {
+    const classes = useStyles();
     const [searchString, setSearchString] = useState('');
     const [showTable, setShowTable] = useState(false);
-    const textBoxRef: any = useRef()
+    const enquiryFilterRef: any = useRef()
 
     return <div className="container">
-        <input type="text" ref={textBoxRef}/>
-        <Button variant="contained"
-                color="primary"
-                onClick={() => {
-                    setSearchString(textBoxRef.current.value)
-                    const stringValue = textBoxRef.current.value.trim();
-                    if (searchString !== stringValue && stringValue.length > 0)
-                        setShowTable(true)
-                }}
-        >
-            Search
-        </Button>
+        <form className={classes.root} noValidate autoComplete="off">
+            <div>
+                <TextField
+                    id="enquiryId"
+                    label="Enquiry"
+                    inputRef={enquiryFilterRef}
+                    variant="outlined"
+                    aria-label={'Enquiry Filter'}
+                />
+            </div>
+
+            <div>
+                <Button variant="contained"
+                        color="primary"
+                        onClick={() => {
+                            setSearchString(enquiryFilterRef.current.value)
+                            const stringValue = enquiryFilterRef.current.value.trim();
+                            if (searchString !== stringValue && stringValue.length > 0)
+                                setShowTable(true)
+                        }}
+                >
+                    Search
+                </Button>
+            </div>
+        </form>
 
 
-        {showTable && <Enquiries searchString={textBoxRef.current.value.trim()}/>}
+        {showTable && <Enquiries searchString={enquiryFilterRef.current.value.trim()}/>}
 
 
     </div>;
